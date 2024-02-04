8d875ef7ede4486bfb9ad151a5466899a0cdc966
Nurdin <mrsunnysummer@gmail.com>
2024-02-04 23:18:40 +0700

    add react routes and create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // * import router
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 export default function App() {
+  // ! Membuat routing file pada react component
   return (
-    <h1 className="text-red-900">App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+
+      </Routes>
+      
+    </BrowserRouter>
   )
 }
