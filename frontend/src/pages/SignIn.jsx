997a218f13b78665b4cfcd1885fc46d44e07be45
Nurdin <mrsunnysummer@gmail.com>
2024-02-16 14:34:22 +0700

    fix in sign in and sign up dom

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -7,18 +7,15 @@ import OAuth from "../components/OAuth.jsx";
 import swal from "sweetalert2";
 
 export default function SignIn() {
-    const [request, setRequest] = useState({});
+    const [formData, setFormData] = useState({});
     // mengambil data statenya dari reducer, "user" disini diambil dari reducer yang ada di store
     const { loading, errors} = useSelector((state) => state.user);
     const navigate = useNavigate(); // navigate disini untuk menavigasi setelah login akan kemana
     const dispatch = useDispatch(); // 
 
-    const handleChange = () => {
-        setRequest({
-            username: document.getElementById("username").value,
-            password: document.getElementById("password").value,
-        });
-        console.log(request);
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.id] : e.target.value});
+        console.log(formData);
     }
 
     const handleClick = async (event) => {
@@ -32,7 +29,7 @@ export default function SignIn() {
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify(request)
+                body: JSON.stringify(formData)
             });
 
             const data = await response.json();
@@ -80,15 +77,15 @@ export default function SignIn() {
     }
 
     return (
-        <div className="h-screen px-4 max-w-lg mx-auto mt-28">
-            <h1 className="text-3xl text-center font-bold my-16 font-rubik">Masuk</h1>
+        <div className="h-screen max-w-lg px-4 mx-auto mt-28">
+            <h1 className="my-16 text-3xl font-bold text-center font-rubik">Masuk</h1>
             <form className="flex flex-col gap-4">
-                <input className="bg-slate-100 p-3 rounded-xl" onChange={handleChange} type="text" id="username" name="username" placeholder="Username" />
-                <input className="bg-slate-100 p-3 rounded-xl" onChange={handleChange} type="password" id="password" name="password" placeholder="Password" />
-                <button disabled={loading} onClick={handleClick} className="bg-slate-900 p-3 rounded-xl text-white hover:opacity-95">{loading ? "Loading..." : "Masuk"}</button>
+                <input className="p-3 bg-slate-100 rounded-xl" onChange={handleChange} type="text" id="username" name="username" placeholder="Username" />
+                <input className="p-3 bg-slate-100 rounded-xl" onChange={handleChange} type="password" id="password" name="password" placeholder="Password" />
+                <button disabled={loading} onClick={handleClick} className="p-3 text-white bg-slate-900 rounded-xl hover:opacity-95">{loading ? "Loading..." : "Masuk"}</button>
                 <OAuth />
             </form>
-            <div className="flex gap-2 mt-5 justify-center">
+            <div className="flex justify-center gap-2 mt-5">
                 <p>Belum punya akun?</p>
                 <Link to="/sign-up">
                     <span className="text-blue-500">Daftar</span>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -8,13 +8,8 @@ export default function SignUp() {
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate(); // navigate disini untuk menavigasi setelah login akan kemana
 
-    const handleChange = () => {
-        setFormData({
-            username: document.getElementById("username").value,
-            email: document.getElementById("email").value,
-            name: document.getElementById("name").value,
-            password: document.getElementById("password").value
-        });
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.id] : e.target.value});
     }
 
 
@@ -59,25 +54,25 @@ export default function SignUp() {
 
     console.log(formData);
     return (
-        <div className="px-4 max-w-lg mx-auto mb-10 mt-28">
-            <h1 className="text-3xl text-center font-bold my-16 font-rubik">Daftar</h1>
+        <div className="max-w-lg px-4 mx-auto mb-10 mt-28">
+            <h1 className="my-16 text-3xl font-bold text-center font-rubik">Daftar</h1>
             {/* Ketika di submit akan menjalan function  */}
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4 justify-center">
-                <input type="text" name="username" id="username" placeholder="Username" className="bg-slate-100 p-3 rounded-xl" onChange={handleChange} />
-                <input type="text" name="email" id="email" placeholder="Email" className="bg-slate-100 p-3 rounded-xl" onChange={handleChange} />
-                <input type="text" name="name" id="name" placeholder="Name" className="bg-slate-100 p-3 rounded-xl" onChange={handleChange} />
-                <input type="password" name="password" id="password" placeholder="Password" className="bg-slate-100 p-3 rounded-xl" onChange={handleChange} />
+            <form onSubmit={handleSubmit} className="flex flex-col justify-center gap-4">
+                <input type="text" name="username" id="username" placeholder="Username" className="p-3 bg-slate-100 rounded-xl" onChange={handleChange} />
+                <input type="text" name="email" id="email" placeholder="Email" className="p-3 bg-slate-100 rounded-xl" onChange={handleChange} />
+                <input type="text" name="name" id="name" placeholder="Name" className="p-3 bg-slate-100 rounded-xl" onChange={handleChange} />
+                <input type="password" name="password" id="password" placeholder="Password" className="p-3 bg-slate-100 rounded-xl" onChange={handleChange} />
                 {/* jika loading true maka akan di disabled */}
-                <button disabled={loading} className="bg-slate-800 text-white p-3 rounded-xl hover:opacity-95">
+                <button disabled={loading} className="p-3 text-white bg-slate-800 rounded-xl hover:opacity-95">
                     {/* jika tidak di klik maka loading akan false dan jika di klik loading akan true */}
                     {loading ? "Loading..." : "Daftar"}
                 </button>
                 <OAuth />
             </form>
-            <p className="text-red-500 text-center mt-4">
+            <p className="mt-4 text-center text-red-500">
                 {/* {errors ? errors : ""} */}
             </p>
-            <div className="flex gap-2 mt-5 justify-center" id="foot">
+            <div className="flex justify-center gap-2 mt-5" id="foot">
                 <p>Sudah punya akun?</p>
                 <Link to="/sign-in">
                     <span className="text-blue-500">Masuk</span>
